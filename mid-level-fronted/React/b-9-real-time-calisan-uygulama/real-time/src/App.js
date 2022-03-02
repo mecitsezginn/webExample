aa708d0e36eb27d101b7c534f5a15e784763975d
Mecit SEZGİN <mecitsezgin55@gmail.com>
2022-03-02 10:39:06 +0300

    yeni dökümanalar

---
+++ mid-level-fronted/React/b-9-real-time-calisan-uygulama/real-time/src/App.js
@@ -0,0 +1,21 @@
+import {init,subscribe} from './socketApi';
+import { useEffect,useState } from 'react';
+import './App.css';
+import Palette from './components/Palette';
+
+function App() {
+  const [activeColor, setActiveColor] = useState("#282c34")
+  useEffect(()=>{
+    init();
+    subscribe((color) =>{
+      setActiveColor(color)
+    });
+  },[])
+  return (
+    <div className="App" style={{backgroundColor: activeColor}}>
+      <Palette activeColor={activeColor}/>
+    </div>
+  );
+}
+
+export default App;
